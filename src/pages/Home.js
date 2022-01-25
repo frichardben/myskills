3b88db1dea5378ab47e37f70110dd94610123e0c
Richard Ben <frichardben@yahoo.com.br>
2022-01-25 11:11:26 -0300

    feat: added state skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -9,6 +9,13 @@ import {
 } from "react-native";
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState("");
+  const [mySkill, setMySkill] = useState([]);
+
+  function handleNewAddSkill() {
+    setMySkill((oldState) => [...oldState, newSkill]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Richard</Text>
@@ -16,12 +23,27 @@ export function Home() {
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
+        onChangeText={setNewSkill}
       />
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleNewAddSkill}
+      >
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, { marginTop: 48 }]}>My Skill</Text>
+      <Text style={[styles.title, { marginVertical: 48 }]}>My Skill</Text>
+
+      {mySkill.map((skill, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.buttonSkill}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textSkill}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -51,10 +73,23 @@ const styles = StyleSheet.create({
     padding: 12,
     borderRadius: 8,
     alignItems: "center",
+    marginTop: 20,
   },
   buttonText: {
     color: "#fff",
     fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonSkill: {
+    backgroundColor: "#1f1e25",
+    padding: 20,
+    borderRadius: 50,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: "#fff",
+    fontSize: 18,
     fontWeight: "bold",
   },
 });

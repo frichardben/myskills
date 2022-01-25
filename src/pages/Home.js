7ed1d7fd47468453640f55902e8fd570b7d60fbf
Richard Ben <frichardben@yahoo.com.br>
2022-01-25 10:39:07 -0300

    feat: added Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,60 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 
 export function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>React Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Richard</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 48 }]}>My Skill</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 16,
+    padding: Platform.OS === "ios" ? 16 : 12,
+    marginTop: 28,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: "#a370f7",
+    padding: 12,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
